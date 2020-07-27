cb9617cd559fe3a8cd387c0bfb3d9cfa97830d8f
Daniel Roth <daniel@firesideagency.ca>
2020-07-27 15:57:47 -0400

    Updated class to match parent and added proper namespace.

--- test/helpers/SnakeCase_PHPUnit_Framework_TestCase.php
+++ test/helpers/SnakeCase_PHPUnit_Framework_TestCase.php
@@ -1,5 +1,5 @@
 <?php
-class SnakeCase_PHPUnit_Framework_TestCase extends PHPUnit_Framework_TestCase
+class SnakeCase_PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase
 {
 	public function __call($meth, $args)
 	{
@@ -13,13 +13,13 @@ class SnakeCase_PHPUnit_Framework_TestCase extends PHPUnit_Framework_TestCase
 		die("PHP Fatal Error:  Call to undefined method $class_name::$meth() in {$trace[1]['file']} on line {$trace[1]['line']}" . PHP_EOL);
 	}
 
-	public function setUp()
+	public function setUp(): void
 	{
 		if (method_exists($this,'set_up'))
 			call_user_func_array(array($this,'set_up'),func_get_args());
 	}
 
-	public function tearDown()
+	public function tearDown(): void
 	{
 		if (method_exists($this,'tear_down'))
 			call_user_func_array(array($this,'tear_down'),func_get_args());
@@ -33,7 +33,7 @@ class SnakeCase_PHPUnit_Framework_TestCase extends PHPUnit_Framework_TestCase
 		return array($keys,$array);
 	}
 
-	public function assert_has_keys(/* $keys..., $array */)
+	public function assert_has_keys(/* $keys..., $array */): void
 	{
 		list($keys,$array) = $this->setup_assert_keys(func_get_args());
 
@@ -43,7 +43,7 @@ class SnakeCase_PHPUnit_Framework_TestCase extends PHPUnit_Framework_TestCase
 			$this->assert_array_has_key($name,$array);
 	}
 
-	public function assert_doesnt_has_keys(/* $keys..., $array */)
+	public function assert_doesnt_has_keys(/* $keys..., $array */): void
 	{
 		list($keys,$array) = $this->setup_assert_keys(func_get_args());
 
@@ -51,12 +51,12 @@ class SnakeCase_PHPUnit_Framework_TestCase extends PHPUnit_Framework_TestCase
 			$this->assert_array_not_has_key($name,$array);
 	}
 
-	public function assert_is_a($expected_class, $object)
+	public function assert_is_a($expected_class, $object): void
 	{
 		$this->assert_equals($expected_class,get_class($object));
 	}
 
-	public function assert_datetime_equals($expected, $actual)
+	public function assert_datetime_equals($expected, $actual): void
 	{
 		$this->assert_equals($expected->format(DateTime::ISO8601),$actual->format(DateTime::ISO8601));
 	}
